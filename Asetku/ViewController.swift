145e1d21c9e812499628cff4664357a7eafca4f9
yonathan <yonathan@asetku.com>
2023-10-18 11:03:33 +0800

    Initial Commit

---
+++ Asetku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Asetku
+//
+//  Created by Jercan on 2023/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
